08f5bd49f741a783b41f378990ee4ffedbf19326
peppem97 <mariobros.gm@gmail.com>
2023-02-20 16:02:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, FormControl, MenuItem, Select, Typography } from "@mui/material";
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
+  const [age, setAge] = useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Typography variant="h2" gutterBottom className="text-center">
+            Estrattore Alunni
+          </Typography>
+          <Row>
+            <Col xl={9}>
+              <FormControl fullWidth>
+              <Select
+                value={age}
+                onChange={handleChange}
+              >
+                <MenuItem value={'TPSIT - 3H'}>TPSIT - 3H</MenuItem>
+                <MenuItem value={'TPSIT - 4H'}>TPSIT - 4H</MenuItem>
+                <MenuItem value={'SISTEMI E RETI - 3F'}>SISTEMI E RETI - 3F</MenuItem>
+                <MenuItem value={'SISTEMI E RETI - 4F'}>SISTEMI E RETI - 4F</MenuItem>
+                <MenuItem value={'SISTEMI E REIT - 5F'}>SISTEMI E REIT - 5F</MenuItem>
+              </Select>
+              </FormControl>
+            </Col>
+            <Col xl={3}></Col>
+          </Row>
+        </Row>
+      </Container>
+    </>
   );
 }
 
